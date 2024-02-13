3d438de34d7ea9916c179b72f1cd058160a20320
Klaytheunstablemf <152714958+Klaytheunstablemf@users.noreply.github.com>
2024-02-14 01:09:38 +0800

    Add files via upload

---
+++ valentine-button.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('button');
+
+// Add an event listener to the button that listens for the click event
+button.addEventListener('click', function() {
+  // Display the prompt when the button is clicked
+  prompt("im gonna be sad :<")
+});
